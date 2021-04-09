be190cca178ed19d6503d79c21bff8198621faa2
colino17 <51090962+colino17@users.noreply.github.com>
2021-04-09 11:45:46 -0300

    Update rename-for-overwrite.sh

--- xbox-music-scripts/rename-for-overwrite.sh
+++ xbox-music-scripts/rename-for-overwrite.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # script for renaming files based on the names of files in a different directory
+# used for modding Amped2 to replace the default wma songs with new songs
+# new songs can be bulk renamed to the original file names so they can overwrite the old songs
+
 # "old" directory is for the files that you want to use the names of
 # "new" directory is for the files that you want to rename using the old names
 # "out" directory is the output directory
