b4ea976eeface792264809afa93184e950adc34e
colino17 <51090962+colino17@users.noreply.github.com>
2021-06-03 20:09:34 -0700

    Add files via upload

---
+++ ffmpeg-scripts/convert-to-hevc-with-filters.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ffmpeg -i "$i" -c:v libx265 -crf 23 -preset fast -c:a aac -strict -2 -movflags faststart "out/${i%.*}.mp4"
+# "-strict -2 flags may be needed for older FFMPEDG versions when using AAC conversion
+
+mkdir out
+
+for i in *.mp4;
+  do name=`echo "${i%.*}"`
+  echo "$name"
+  audioformat=$(ffprobe -loglevel error -select_streams a:0 -show_entries stream=codec_name -of default=nw=1:nk=1 "$i")
+    if [ "$audioformat" = "aac" ]; then
+      ffmpeg -i "$i" -vf "eq=contrast=1.2:saturation=1.5,unsharp,hqdn3d,yadif" -c:v libx265 -crf 23 -preset fast -c:a copy -movflags faststart "out/${i%.*}.mp4"
+    else
+      ffmpeg -i "$i" -vf "eq=contrast=1.2:saturation=1.5,unsharp,hqdn3d,yadif" -c:v libx265 -crf 23 -preset fast -c:a aac -movflags faststart "out/${i%.*}.mp4"
+    fi
+done
