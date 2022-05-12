2bb19024c4a402f8b8f91f67b63fde552461933c
colino17 <51090962+colino17@users.noreply.github.com>
2022-05-11 23:50:03 -0300

    Update remove-audio-tracks.sh

--- ffmpeg-scripts/remove-audio-tracks.sh
+++ ffmpeg-scripts/remove-audio-tracks.sh
@@ -4,4 +4,4 @@
 # Note that to retain track 1 the variable used should be 0, track 2 the variable would be 1, etc
 # Usage "sh remove-audio-tracks.sh inputfilename audiotracktokeep outputfilename"
 
-ffmpeg -i $1 -sn -map 0:v:0 -map 0:a:$2 -c copy $3
+ffmpeg -i $1 -sn -map_metadata -1 -map 0:v:0 -map 0:a:$2 -c copy $3
